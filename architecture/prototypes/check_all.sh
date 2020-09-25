6bcd821617b6e7b46ba5e558023c2acd8d5f863a
David Johnston <dwtj@dwtj.me>
2020-09-25 13:32:40 -0700

    Add a script to check all architecture prototypes

---
+++ architecture/prototypes/check_all.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -
+#
+# This script builds and runs some very basic tests on the architecture
+# prototypes. It should be invoked from the root of the repository.
+
+set -e
+
+REPOSITORY_ROOT="$PWD"
+
+cd "${REPOSITORY_ROOT}/architecture/prototypes/instrumentation/pt_asm_java_agent"
+bazel build //...
+bazel test //...
+
+# TODO(dwtj): This way of invoking the client and server is a an ugly and flaky
+#  hack. Consider fixing it and doing it properly.
+cd "${REPOSITORY_ROOT}/architecture/prototypes/integration/pt_grpc"
+bazel build //...
+timeout 5s bazel-bin/pt/my_server/my_server &
+sleep 1s
+bazel run //pt/my_client
+
+cd "${REPOSITORY_ROOT}/architecture/prototypes/benchmarking/pt_renaissance"
+bazel build //...
+bazel run //third_party/renaissance:renaissance_exec -- --repetitions 1 scrabble
+bazel test //...
