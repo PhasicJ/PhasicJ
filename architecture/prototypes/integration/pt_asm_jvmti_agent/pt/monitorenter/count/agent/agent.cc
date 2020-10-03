55354271f3f3e1a5a8f1719f6e1045e3f47597f1
David Johnston <dwtj@dwtj.me>
2020-10-02 23:56:47 -0700

    Add stub JVMTI agent to `pt_asm_jvmti_agent`
    This JVMTI agent currently just prints a message to STDOUT on load.

--- architecture/prototypes/integration/pt_asm_jvmti_agent/pt/monitorenter/count/agent/agent.cc
+++ architecture/prototypes/integration/pt_asm_jvmti_agent/pt/monitorenter/count/agent/agent.cc
@@ -1,5 +1,16 @@
+#include <iostream>
+
+#include "jni.h"
+#include "jvmti.h"
+
 namespace pt::monitorenter::count::agent {
 
-  // TODO(dwtj): Everything!
+// TODO(dwtj): Everything!
 
 }  // namespace pt::monitorenter::count::agent
+
+JNIEXPORT jint JNICALL
+Agent_OnLoad(JavaVM *vm, char *options, void *reserved) {
+  std::cout << "Hello, from JVMTI." << std::endl;
+  return 0;
+}
