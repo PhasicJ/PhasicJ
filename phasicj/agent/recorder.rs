22c2458c7087f5f7f000e2664e2fb5bd421e2b0b
David Johnston <dwtj@dwtj.me>
2021-07-06 11:39:52 -0700

    Submit agent's forwarded events to a Tokio runtime
    Previously, they were pushed into a channel.
    
    This change was made to prepare for use of a Tonic gRPC client running
    in a Tokio runtime.

--- phasicj/agent/recorder.rs
+++ phasicj/agent/recorder.rs
@@ -6,33 +6,23 @@ use std::sync::Mutex;
 use lazy_static::lazy_static;
 use std::thread;
 use std::sync::mpsc::channel;
+use tokio::runtime;
 
 lazy_static! {
     // TODO(): Events sent into this channel will be forwarded to the daemon via
     // gRPC over UDP.
-    static ref EVENT_FORWARDING_CHANNEL: Mutex<Option<Sender<()>>> = Mutex::new(None);
+    static ref EVENT_FORWARDING_RUNTIME: runtime::Runtime =
+        runtime::Builder::new_multi_thread()
+            .worker_threads(2)
+            .thread_name("grpc_recorder_client")
+            .build()
+            .unwrap();
 }
 
 // This is meant to be used by event observation code within the agent to
 // forward events out of the agent.
 pub fn forward_event(event: ()) {
-    EVENT_FORWARDING_CHANNEL.lock().unwrap().as_ref().unwrap().send(()).unwrap();
-}
-
-// This is meant to be called once during the agent's initial startup before
-// the user's JVM application code has started to run.
-pub fn start_event_forwarding_single_threaded_tokio_runtime() {
-    // Initialize the event channel.
-    let (tx, rx) = channel();
-    {
-        let mut data = EVENT_FORWARDING_CHANNEL.lock().unwrap();
-        *data = Some(tx);
-    }
-
-    thread::spawn(move || {
-        loop {
-            let msg = rx.recv().unwrap();
-            log::trace!("Observed an Event!");
-        }
+    EVENT_FORWARDING_RUNTIME.spawn(async move {
+        log::trace!("Forwarded an event: {:?}", event)
     });
 }
